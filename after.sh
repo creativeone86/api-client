258174dc001c2d368f9b2950044dbc1998d0039f
Yordan Georgiev <iordan340@gmail.com>
2018-01-31 10:58:17 +0200

    Improved build process, now xdebug build added plus assets build

--- after.sh
+++ after.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 # -- xdebug related scripts --
-# wget http://xdebug.org/files/xdebug-2.6.0RC2.tgz && tar -xvzf xdebug-2.6.0RC2.tgz && xdebug-2.6.0RC2.tgz
-# cd xdebug-2.6.0RC2
-# phpize
-# ./configure
-# make
-# sudo cp modules/xdebug.so /usr/lib/php/20170718
+wget http://xdebug.org/files/xdebug-2.6.0RC2.tgz && tar -xvzf xdebug-2.6.0RC2.tgz && xdebug-2.6.0RC2.tgz
+cd xdebug-2.6.0RC2
+phpize
+./configure
+make
+sudo cp modules/xdebug.so /usr/lib/php/20170718
+sudo cp ../code/20-xdebug.ini.example /etc/php/7.2/fpm/conf.d/20-xdebug.ini
+sudo service php7.2-fpm restart
 
-# -- Edit /etc/php/7.2/cli/php.ini and add the line --
+# -- for reference /etc/php/7.2/cli/php.ini  --
 # zend_extension = /usr/lib/php/20170718/xdebug.so
 
 # -- project specific scripts --
@@ -17,8 +19,9 @@ yarn
 sudo npm install npm@latest -g
 
 # laravel specific
-cd code
+cd ../code
 cp .env.example .env
 php artisan key:generate
 php artisan migrate
 npm i
+npm run dev
